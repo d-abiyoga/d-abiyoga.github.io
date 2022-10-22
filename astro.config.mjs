7d35df62d3e5db895d219f003f45a14376097e8c
dimas abiyoga <dimas.abiyoga169@gmail.com>
2022-10-22 10:23:36 +0700

    remove base url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://d-abiyoga.github.io',
-    base: '/',
 	integrations: [mdx(), sitemap()],
 });
